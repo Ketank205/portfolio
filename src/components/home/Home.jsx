f2207dfc006f71c659c000e8cf0bf6b6c78675a2
Ketankumar Borse <ketankumarpborse2020@gmail.com>
2024-09-09 22:40:55 +0530

    Fourth Commit : Home Page Done

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./home.css";
 import Social from './Social';
 import Data from './Data';
+import ScrollDown from './ScrollDown';
 const Home = () => {
     return (
         <>
@@ -11,7 +12,10 @@ const Home = () => {
                     <Social/>
                     <div className="home__img"></div>
                     <Data/>
+
                 </div>
+
+                <ScrollDown/>   
             </div>
         </section>
         </>
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,13 +4,13 @@ const Social = () =>{
     return(
         <div className="home__social">
             <a href="https://www.linkedin.com/in/ketankumar-borse-470a06259/" className="home__social-icon" target="_blank">
-            <i class="uil uil-linkedin-alt"></i>
+            <i className="uil uil-linkedin-alt"></i>
             </a>
             <a href="https://github.com/Ketank205" className="home__social-icon" target="_blank">
-            <i class="uil uil-github-alt"></i>
+            <i className="uil uil-github-alt"></i>
             </a>
             <a href="https://www.instagram.com" className="home__social-icon" target="_blank">
-            <i class="uil uil-instagram"></i>
+            <i className="uil uil-instagram"></i>
             </a>
         </div>
     );
